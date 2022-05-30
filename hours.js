8192c971230539a487655d123af62d2e36848de2
sudais <127001sudais01@gmail.com>
2022-05-30 15:38:41 +0800

    first commit

---
+++ hours.js
@@ -0,0 +1,16 @@
+let first_week = 6.50 + 6.50 + 6.50 + 6.50 + 6.50
+let second_week = 5.50
+let third_week = 5 + 6.50
+let fourth_week = 5.50 + 5.50 + 9 + 5.50 + 6.50 + 9.50
+let fifth_week = 5.50
+
+
+
+console.log("first week hrs: ", first_week)
+console.log("second week hrs: ", second_week)
+console.log("third week hrs: ", third_week)
+console.log("fourth week hrs: ", fourth_week)
+console.log("fifth week hrs: ", fifth_week)
+let total_hours = first_week + second_week + third_week + fourth_week + fifth_week
+console.log("total hours: ", total_hours)
+
